ad81dcf7ca816b178d9075d99873680a6184f808
40261845 <dcollado01@qub.ac.uk>
2023-07-27 16:45:15 +0100

    Simple notifications are looking really good

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import FirebaseAuth
+import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +9,21 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+     if #available(iOS 10.0, *) {
+              UNUserNotificationCenter.current().delegate = self
+      }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // https://stackoverflow.com/questions/57048800/flutter-if-app-delegate-swizzling-is-disabled-remote-notifications-received-by
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    // Pass device token to auth
+    Auth.auth().setAPNSToken(deviceToken, type: .unknown)
+
+    // Pass device token to messaging
+    Messaging.messaging().apnsToken = deviceToken
+
+    return super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
   }
 }
