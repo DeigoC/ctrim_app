f45a027d3ec4668bb508e63a78102695288a9704
40261845 <dcollado01@qub.ac.uk>
2023-07-27 15:00:36 +0100

    Getting ready for Functions testing for FCM

--- functions/main.py
+++ functions/main.py
@@ -2,12 +2,91 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+from firebase_functions import https_fn, options
+from firebase_admin import initialize_app, messaging
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+initialize_app()
+options.set_global_options(max_instances=10, region='europe-west1')
+
+@https_fn.on_call(region='europe-west1')
+def send_notification_to_multiple_tokens(req: https_fn.CallableRequest) -> any:
+    # title = req.data['Title']
+    # body = req.data['Body']
+    # tokens = req.data['Tokens']
+    # data = req.data['Data']
+    if (str(req.data['Tokens']).find(',') != -1 ):    
+        tokens = str(req.data['Tokens']).split(',')
+    else:
+        tokens = [str(req.data['Tokens'])]
+   
+    dataDict = {};
+
+    iosImage = str(req.data['iOSImage'])
+    androidImage = str(req.data['AndroidImage'])
+
+    if (str(req.data['DataKeys']).find(',') != -1 ):   
+        dataKeys = str(req.data['DataKeys']).split(',')
+        dataValues = str(req.data['DataValues']).split(',')
+        for i in range(len(dataKeys)):
+            dataDict[dataKeys[i]] = dataValues[i]
+    else:
+        dataDict = {str(req.data['DataKeys']) : str(req.data['DataValues'])}
+
+    print('--------------- Tokens are ' + str(tokens))
+    print('--------------- DataDict is ' + str(dataDict))
+    msg = messaging.MulticastMessage(
+        tokens=tokens,
+        data=dataDict,
+        notification=messaging.Notification(
+            title=req.data['Title'],
+            body=req.data['Body']
+        ),
+        apns=messaging.APNSConfig(
+            payload=messaging.APNSPayload(aps=messaging.Aps(mutable_content=True)),
+            fcm_options=messaging.APNSFCMOptions(image=iosImage)
+        ),
+        android=messaging.AndroidConfig(notification=messaging.AndroidNotification(image=androidImage))
+    )
+
+    messaging.send_multicast(msg)
+    return {'result':'finished sending to multiple devices!'}
+
+@https_fn.on_call(region='europe-west1')
+def send_to_topic(req: https_fn.CallableRequest) -> any:
+    topic = str(req.data['Topic'])
+    dataDict = {};
+    iosImage = str(req.data['iOSImage'])
+    androidImage = str(req.data['AndroidImage'])
+
+    if (str(req.data['DataKeys']).find(',') != -1 ):   
+        dataKeys = str(req.data['DataKeys']).split(',')
+        dataValues = str(req.data['DataValues']).split(',')
+        for i in range(len(dataKeys)):
+            dataDict[dataKeys[i]] = dataValues[i]
+    else:
+        dataDict = {str(req.data['DataKeys']) : str(req.data['DataValues'])}
+
+    print('--------------- Topic is ' + topic)
+    print('--------------- DataDict is ' + str(dataDict))
+    
+    msg = messaging.Message(
+        topic=topic,
+        data=dataDict,
+        notification=messaging.Notification(
+            title=req.data['Title'],
+            body=req.data['Body']
+        ),
+        apns=messaging.APNSConfig(
+            payload=messaging.APNSPayload(aps=messaging.Aps(mutable_content=True)),
+            fcm_options=messaging.APNSFCMOptions(image=iosImage)
+        ),
+        android=messaging.AndroidConfig(notification=messaging.AndroidNotification(image=androidImage))
+    )
+
+    messaging.send(msg)
+    return {'result':'finished sending to topic!'}
+
+@https_fn.on_call(region='europe-west1')
+def hello_world(req: https_fn.CallableRequest) -> any:
+    print('Hello there! Here is the Request data: ' + str(req.data))
+    return{'result':'finished hello world!'}
--- lib/pages/personal_home.dart
+++ lib/pages/personal_home.dart
@@ -1,3 +1,4 @@
+import 'package:ctrim_app/firebase/functions_manager.dart';
 import 'package:ctrim_app/pages/personal/login_page.dart';
 import 'package:ctrim_app/utility/app_context.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,8 @@ class PersonalHome extends StatefulWidget {
 }
 
 class _PersonalHomeState extends State<PersonalHome> {
+  final CloudFunctionManager _functionManager = CloudFunctionManager();
+
   @override
   Widget build(BuildContext context) {
     // ? this may not be needed cause of the Consumer at the page level (home_page)
@@ -58,6 +61,33 @@ class _PersonalHomeState extends State<PersonalHome> {
         //             ))));
         //   },
         // )
+
+        ListTile(
+            title: const Text('Hello World Function'),
+            leading: const Icon(Icons.waving_hand),
+            onTap: () {
+              _functionManager.helloWorld();
+            }),
+        ListTile(
+            title: const Text('Send to Devices'),
+            leading: const Icon(Icons.send_to_mobile),
+            onTap: () {
+              _functionManager.sendMessageToSelectedTokens(
+                  tokens: ['not_a_real_token'],
+                  title: 'This is a test title',
+                  body: "Hopefully this works 🤞, if it does - yeah man! Otherwise, that's unfortuante!",
+                  data: {'data': 'nothing for all devices'});
+            }),
+        ListTile(
+            title: const Text('Send to Topic'),
+            leading: const Icon(Icons.people_alt),
+            onTap: () {
+              _functionManager.sendToTopic(
+                  topic: 'ctrim-belfast',
+                  title: 'This is for BELFAST - Yeahh1!!',
+                  body: "Hopefully this works 🤞, if it does - yeah man! This is for when a new post is uploaded",
+                  data: {'data': 'nothing for this topic'});
+            }),
       ];
 
       if (appContext.isCurrentUserGuest) {
